f8ad31a9e59bc8e876851a22fec2037faf217c03
Murad <muradmustafayev03@gmail.com>
2023-12-02 18:59:24 +0400

    added login endpoint

--- src/server/api/main.py
+++ src/server/api/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request, session
 
 from src.server import DataCollector, face_matching_probability
 
@@ -6,6 +6,19 @@ from src.server import DataCollector, face_matching_probability
 app = Flask(__name__)
 
 
-@app.route('/api/v1/find_person', methods=['POST'])
-def find_person():
-    pass
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    username = data.get('username')
+    password = data.get('password')
+
+    try:
+        DataCollector(username, password)
+    except Exception as e:
+        return jsonify({'message': 'Failed to login', 'error': e}), 401
+
+    session['username'] = username
+    session['password'] = password
+
+    return jsonify({'message': 'Login successful'}), 200
